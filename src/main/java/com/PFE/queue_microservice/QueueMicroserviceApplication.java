e00b8bb2a140ae01426c92c5a498e7744096a4eb
ikhlas <ei_titouche@esi.dz>
2020-10-20 12:03:56 +0100

    initial commit: initial project with no code just dependencies from the initializr

---
+++ src/main/java/com/PFE/queue_microservice/QueueMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.PFE.queue_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PFE/queue_microservice/QueueMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PFE.queue_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
